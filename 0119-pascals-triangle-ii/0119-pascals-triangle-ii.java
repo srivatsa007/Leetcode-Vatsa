5abb0c0e2596d3bad42138030ebaa210192ea378
Srivats <70172502+srivatsa007@users.noreply.github.com>
2023-10-20 05:55:12 +0530

    Time: 0 ms (100%), Space: 40.3 MB (33.31%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+        int row = rowIndex;
+        double ele=1;
+        list.add((int)ele);
+        for(int j=0;j<rowIndex;j++) //starts from index1, j0; index2, j1; 
+        {
+            ele= (ele*(rowIndex-j))/(j+1);
+            list.add((int)ele);
+        }
+        return list;
+    }
+   
+}
+/* 
+WITH ARRAYS
+for(j=0; j<row; j++){
+    arr[j+1]= (arr[j-1]*(row-1))/(j+1);
+}
+return arr;
+DOESNT WORK!!!!!!!!!!!! Check code for array
+
+*/
